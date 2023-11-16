96a28cee7a365b0c465a00a663ed00d8df6f56eb
wendler <wendlerconstancio@gmail.com>
2023-11-15 22:20:42 -0300

    Adicionando Item

--- script.js
+++ script.js
@@ -11,6 +11,30 @@ const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
 
+// Funções
+const saveTodo = (text, done = 0, save = 1) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(deleteBtn);
+
 // Eventos
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -21,3 +45,4 @@ todoForm.addEventListener("submit", (e) => {
       saveTodo(inputValue);
     }
   });
+}
