6125195bf02e685fc9d32f1af3cc6287a4098ac8
wendler <wendlerconstancio@gmail.com>
2023-11-15 22:12:25 -0300

    Iniciando Javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const eraseBtn = document.querySelector("#erase-button");
+const filterBtn = document.querySelector("#filter-select");
+
+let oldInputValue;
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    const inputValue = todoInput.value;
+  
+    if (inputValue) {
+      saveTodo(inputValue);
+    }
+  });
